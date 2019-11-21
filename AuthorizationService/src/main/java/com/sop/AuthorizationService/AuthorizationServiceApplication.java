2cb9168d8ba3fb78789015857797d959f83ad935
nuengnakhap <nueng1569@hotmail.com>
2019-11-21 12:24:33 +0700

    update auth

--- AuthorizationService/src/main/java/com/sop/AuthorizationService/AuthorizationServiceApplication.java
+++ AuthorizationService/src/main/java/com/sop/AuthorizationService/AuthorizationServiceApplication.java
@@ -3,10 +3,11 @@ package com.sop.AuthorizationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class AuthorizationServiceApplication {
+public class AuthorizationServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServiceApplication.class, args);
